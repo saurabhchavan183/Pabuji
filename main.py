d357b6d280e33b3d6f3714caabc2abb05ecd57bc
saurabhchavan183 <118131910+saurabhchavan183@users.noreply.github.com>
2026-05-02 13:31:17 +0530

    Add FastAPI chat endpoint with GenAI integration
    Implement a FastAPI application with a chat endpoint that uses Google GenAI for generating responses based on user input and a system prompt.

---
+++ main.py
@@ -0,0 +1,48 @@
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from google import genai
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],  # your frontend URL
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Request body
+class ChatRequest(BaseModel):
+    message: str
+
+# Initialize client
+client = genai.Client(api_key="AIzaSyDIFk_WynzOhKJ4isiizWfrGDAMpI41PUA")
+
+models = client.models.list()
+# /
+# ---- SYSTEM PROMPT ----
+
+
+@app.post("/chat/{topic}")
+async def chat(topic: str,req: ChatRequest):
+    try:
+        print("top ",topic)
+        SYSTEM_PROMPT = f"""
+        I have created a website on the folktale of Pabuji. It has a chatbot for users to talk to {topic}.
+        You have to answer the user query as {topic} in the context of 'The Epic of Pabuji' by John D Smith.
+        Give direct and short answers.
+        """
+        response = client.models.generate_content(
+            model="gemini-2.5-flash",
+            contents=[
+                {"role": "user", "parts": [{"text": SYSTEM_PROMPT}]},
+                {"role": "user", "parts": [{"text": req.message}]}
+            ]
+        )
+        print(response)
+        return {"response": response.text}
+    except Exception as e:
+        return {"error": str(e)}
